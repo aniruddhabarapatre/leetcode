8159a4be253e0f8a3c69f652d39577819a30e245
Aniruddha Barapatre <aniruddhabarapatre@gmail.com>
2025-10-23 21:07:21 +0530

    lru cache

---
+++ 146_lru_cache.py
@@ -0,0 +1,94 @@
+"""
+Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+Implement the LRUCache class:
+
+LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+int get(int key) Return the value of the key if the key exists, otherwise return -1.
+void put(int key, int value) Update the value of the key if the key exists.
+Otherwise, add the key-value pair to the cache.
+If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+The functions get and put must each run in O(1) average time complexity.
+
+
+Example 1:
+
+Input
+["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+Output
+[null, null, null, 1, null, -1, null, -1, 3, 4]
+
+Explanation
+LRUCache lRUCache = new LRUCache(2);
+lRUCache.put(1, 1); // cache is {1=1}
+lRUCache.put(2, 2); // cache is {1=1, 2=2}
+lRUCache.get(1);    // return 1
+lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+lRUCache.get(2);    // returns -1 (not found)
+lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+lRUCache.get(1);    // return -1 (not found)
+lRUCache.get(3);    // return 3
+lRUCache.get(4);    // return 4
+
+"""
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+
+class Node:
+    def __init__(self, key: int, value: int):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+
+        self.left = Node(0, 0)  # LRU
+        self.right = Node(0, 0)  # MRU
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def _remove(self, node: Node) -> None:
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def _insert(self, node: Node) -> None:
+        # Insert node at right (most recently used)
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.prev = prev
+        node.next = nxt
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            # Move the accessed node to the right (most recently used)
+            node = self.cache[key]
+
+            # update the LRU order
+            self._remove(node)
+            self._insert(node)
+            return node.value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            # Remove the old node
+            self._remove(self.cache[key])
+        node = Node(key, value)
+        self._insert(node)
+        self.cache[key] = node
+
+        if len(self.cache) > self.capacity:
+            # Remove the LRU from the linked list and delete from the cache
+            lru = self.left.next
+            self._remove(lru)
+            del self.cache[lru.key]
