f4b2cff1a56369379e918563f8d8fc2b66920ffd
Aniruddha Barapatre <aniruddhabarapatre@gmail.com>
2025-10-29 20:47:02 +0530

    DEV: possible subsets

---
+++ 78_subsets.py
@@ -0,0 +1,36 @@
+"""
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+"""
+
+from typing import List
+
+
+class Solution:
+    # Time complexity: O(n * 2^n)
+    # Space complexity: O(n * 2^n)
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        subset = []
+
+        def backtrack(start: int):
+            result.append(subset.copy())
+
+            for i in range(start, len(nums)):
+                subset.append(nums[i])
+                backtrack(i + 1)
+                subset.pop()
+
+        backtrack(0)
+        return result
