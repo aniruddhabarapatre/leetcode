402a07c51785969432834e2199457db0d5bf4406
Aniruddha B <aniruddha.barapatre@gmail.com>
2026-04-22 21:18:48 +0530

    DEV: group anagrams

---
+++ 1_array_hashing/49_group_anagrams.py
@@ -0,0 +1,40 @@
+"""
+Given an array of strings strs, group all anagrams together into sublists.
+You may return the output in any order.
+
+An anagram is a string that contains the exact same characters as another string,
+but the order of the characters can be different.
+
+Example 1:
+
+Input: strs = ["act","pots","tops","cat","stop","hat"]
+Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+
+Example 2:
+
+Input: strs = ["x"]
+Output: [["x"]]
+
+Example 3:
+
+Input: strs = [""]
+Output: [[""]]
+"""
+
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    # Time complexity: O(m * n)
+    # Space complexity: O(m * n)
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list)
+
+        for s in strs:
+            counts = [0] * 26
+
+            for char in s:
+                counts[ord(char) - ord("a")] += 1
+            result[tuple(counts)].append(s)
+        return list(result.values())
